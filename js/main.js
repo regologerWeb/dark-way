8f8d345ec65a9fc0dffe7d0cc113ec0dc933387f
regologerWeb <145193653+regologerWeb@users.noreply.github.com>
2023-10-15 19:47:30 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.157.0/three.module.min.js';
-import * as CANNON from 'https://cdnjs.cloudflare.com/ajax/libs/cannon.js/0.6.2/cannon.js';
+import * as CANNON from 'https://cdn.jsdelivr.net/npm/cannon-es@0.20.0/+esm';
 import { PointerLockControlsCannon } from './PointerLockControlsCannon.js'
 
 const instructions = document.getElementById('instructions')
