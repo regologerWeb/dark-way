c0a0536da110d465effe93bbda3d1d8e2404ca0d
regologerWeb <145193653+regologerWeb@users.noreply.github.com>
2023-10-15 19:38:33 +0500

    main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.157.0/three.module.min.js';
-import * as CANNON from 'cannon-es';
+import * as CANNON from '../cannon-es';
 import { PointerLockControlsCannon } from './PointerLockControlsCannon.js'
 
 const instructions = document.getElementById('instructions')
